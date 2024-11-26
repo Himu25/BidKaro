b8727fe1603f29b8f10812717d265e9ad72f0adc
Himanshu singh <99968348+Himu25@users.noreply.github.com>
2024-11-27 02:09:08 +0530

    new code

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,19 @@
-import adapter from "@sveltejs/adapter-vercel";
+import adapter from "@sveltejs/adapter-netlify";
 import preprocess from "svelte-preprocess";
 import { resolve } from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Enable preprocessors
   preprocess: preprocess(),
 
   kit: {
-    // floc: true, // Optional: Enables FLoC (Federated Learning of Cohorts) if required
-    adapter: adapter(), // Vercel adapter for deployment
+    adapter: adapter({
+      fallback: "200.html",
+    }),
+
     vite: {
       resolve: {
         alias: {
-          // Custom aliases for easier imports
           $services: resolve("./src/services"),
         },
       },
